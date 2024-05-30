656c77070a933fe24d07bd12aeb76f18d6a30811
Salikh Gurgenidze <vatia0@gmail.com>
2024-05-30 22:56:35 +0400

    fix: basket items

--- src/components/BasketItems.tsx
+++ src/components/BasketItems.tsx
@@ -1,12 +1,7 @@
 import { TBasketItem } from "../store/basketSlice"
 
 const BasketItems = ({ basket }: { basket: TBasketItem[] }) => {
-  const totalItems = () => {
-    return basket.reduce((a, b) => {
-      return a + b.quantity
-    }, 0)
-  }
-  return <div className="mr-4">Basket Items: {totalItems()}</div>
+  return <div className="mr-4">Basket Items: {basket.length}</div>
 }
 
 export default BasketItems
